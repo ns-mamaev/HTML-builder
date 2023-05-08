273d0f44c703f19ae8d86d82e2048623e19df8cc
ns-mamaev <palmally@yandex.ru>
2023-05-08 20:24:55 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const { copyFile} = require('fs');
+const path = require('path');
+const { readdir, mkdir } = require('fs/promises');
 
+const folderPath = path.resolve(__dirname, 'files');
+
+async function makeDirectory(targetFolder) {
+  const dirCreation = await mkdir(targetFolder, { recursive: true });
+  
+  return dirCreation;
+}
+
+function makeFileCopy(source, target) {
+  copyFile(source, target, (err) => {
+    if (err) throw err;
+  }); 
+}
+
+// main function
+async function makeDirCopy() {
+  const copyFolderName = folderPath + '-copy';
+  try {
+    makeDirectory(copyFolderName);
+    const items = await readdir(folderPath, { withFileTypes: true });
+    for (const item of items) {
+      const { name } = item;
+      if (item.isFile()) {
+        const source = path.resolve(folderPath, name);
+        const target = path.resolve(copyFolderName, name);
+        makeFileCopy(source, target);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+makeDirCopy();
